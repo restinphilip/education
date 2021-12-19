5d55a7c24e7b0ac5ede1ac60c9ffe9c130c6b356
resrtinphilip <restinphilip@gmail.com>
2021-12-19 07:43:30 +0000

    updated

---
+++ updateSubject.php
@@ -0,0 +1,76 @@
+<?php
+    include 'functions.php';
+    if(empty($_SESSION['suuser']))
+    {
+      echo "<script>location.href='index.php';</script>";
+      exit;
+    }
+    getHeader();
+    getSideBar();
+    $subjectId = $_GET['row_id'];
+    $subjectData = getSubjectDataById($subjectId);
+    $subjectData = mysqli_fetch_array($subjectData);
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Update Subject</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Update Subject</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form action="" method="post" enctype="multipart/form-data">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter name" value="<?php echo $subjectData['name']; ?>" required>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+          </div>
+          <!--/.col (left) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php getFooter(); ?>
+  <?php
+    if(isset($_POST['submit']))
+    {
+      $name = $_POST['name'];
+      updateSubject($subjectId,$name);
+    }
+  ?>
